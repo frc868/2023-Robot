73bb73218a070bede78c9084f53617124811cc82
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-09-10 09:38:42 -0400

    Update CANCoder IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,25 +24,25 @@ public final class Constants {
             public static final class FrontLeft {
                 public static final int DRIVE_MOTOR = 1;
                 public static final int TURN_MOTOR = 2;
-                public static final int TURN_ENCODER = 9;
+                public static final int TURN_ENCODER = 1;
             }
 
             public static final class FrontRight {
                 public static final int DRIVE_MOTOR = 3;
                 public static final int TURN_MOTOR = 4;
-                public static final int TURN_ENCODER = 10;
+                public static final int TURN_ENCODER = 2;
             }
 
             public static final class BackLeft {
                 public static final int DRIVE_MOTOR = 5;
                 public static final int TURN_MOTOR = 6;
-                public static final int TURN_ENCODER = 11;
+                public static final int TURN_ENCODER = 3;
             }
 
             public static final class BackRight {
                 public static final int DRIVE_MOTOR = 7;
                 public static final int TURN_MOTOR = 8;
-                public static final int TURN_ENCODER = 12;
+                public static final int TURN_ENCODER = 4;
             }
         }
 
