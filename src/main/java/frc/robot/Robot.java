24e35f8e335278623cb5125806200b4dc412b419
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-01-04 20:20:23 -0500

    add houndutil stuff to robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import com.techhounds.houndutil.houndlib.auto.AutoManager;
 import com.techhounds.houndutil.houndlog.LoggingManager;
 
 /**
@@ -20,10 +21,6 @@ import com.techhounds.houndutil.houndlog.LoggingManager;
  * project.
  */
 public class Robot extends TimedRobot {
-    private Command m_autonomousCommand;
-
-    private RobotContainer m_robotContainer;
-
     /**
      * This function is run when the robot is first started up and should be used
      * for any
@@ -33,13 +30,12 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our autonomous chooser on the dashboard.
-        m_robotContainer = new RobotContainer();
+        new RobotContainer();
         LoggingManager.getInstance().init();
 
-        // This is something you won't see often in FRC, but I wanted to set the
-        // LoggingManager to run every 100ms instead of every 10ms, and on a 5ms offset
-        // from robotPeriodic.
-        addPeriodic(LoggingManager.getInstance()::run, 0.1, 0.005);
+        // sets the LoggingManager to run every 100ms and on an offset of 10ms from the
+        // main thread
+        addPeriodic(LoggingManager.getInstance()::run, 0.1, 0.010);
 
         LiveWindow.disableAllTelemetry();
     }
@@ -61,6 +57,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void disabledPeriodic() {
+        AutoManager.getInstance().updateShuffleboard();
     }
 
     /**
@@ -69,12 +66,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousInit() {
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-        // schedule the autonomous command (example)
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.schedule();
-        }
+        AutoManager.getInstance().runSelectedRoutine();
     }
 
     /** This function is called periodically during autonomous. */
@@ -84,13 +76,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.cancel();
-        }
+        AutoManager.getInstance().endRoutine();
     }
 
     /** This function is called periodically during operator control. */
