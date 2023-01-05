12921c769117fc54220905f67f098640d29db0ec
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-01-04 20:17:55 -0500

    add static auto routine builder

---
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
+
+public class AutoRoutines {
+
+    public static Command generateSwervePathFollowingCommand(PathPlannerTrajectory path, Drivetrain drivetrain) {
+        return new PPSwerveControllerCommand(
+                path,
+                drivetrain::getPose,
+                Constants.Drivetrain.Geometry.KINEMATICS,
+                new PIDController(Constants.Drivetrain.PID.Trajectories.xkP, 0, 0),
+                new PIDController(Constants.Drivetrain.PID.Trajectories.ykP, 0, 0),
+                new PIDController(Constants.Drivetrain.PID.Trajectories.thetakP, 0, 0),
+                (s) -> drivetrain.setModuleStates(s, true, true),
+                drivetrain);
+    }
+
+    public static Command generateSpinningSwervePathFollowingCommand(PathPlannerTrajectory path,
+            Drivetrain drivetrain) {
+        return new PPSwerveControllerCommand(
+                path,
+                drivetrain::getPose,
+                Constants.Drivetrain.Geometry.KINEMATICS,
+                new PIDController(Constants.Drivetrain.PID.Trajectories.xkP, 0, 0),
+                new PIDController(Constants.Drivetrain.PID.Trajectories.ykP, 0, 0),
+                new PIDController(0, 0, 0),
+                (s) -> {
+                    ChassisSpeeds c = Constants.Drivetrain.Geometry.KINEMATICS.toChassisSpeeds(s);
+                    c.omegaRadiansPerSecond = Math.PI;
+
+                    drivetrain.setModuleStates(Constants.Drivetrain.Geometry.KINEMATICS.toSwerveModuleStates(c), true,
+                            true);
+                },
+                drivetrain);
+    }
+}
