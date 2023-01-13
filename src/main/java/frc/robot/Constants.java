0a56c0636d21c02a31ce8612b0b1f56d03fc466d
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-01-13 15:11:49 -0500

    Add vision constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,9 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
@@ -147,6 +150,13 @@ public final class Constants {
          */
         public static final double JOYSTICK_INPUT_RATE_LIMIT = 15.0;
 
+    }
+
+    public static final class Vision {
+        public static final String CAMERA_NAME = "OV9281-FWD";
+        public static final Transform3d ROBOT_TO_CAM = new Transform3d(
+                new Translation3d(0.24, 0.0, 0.0),
+                new Rotation3d(0, 0, 0));
     }
 
     public static final class Auton {
