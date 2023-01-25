7d031013604be02369564ff3919f82ae2c7d30b7
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-01-25 17:30:23 -0500

    add untested to all untested constnats

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,91 +28,80 @@ public final class Constants {
     public static final class Drivetrain {
         public static final class CANIDs {
             public static final class FrontLeft {
-                public static final int DRIVE_MOTOR = 1;
-                public static final int TURN_MOTOR = 2;
-                public static final int TURN_ENCODER = 0;
+                public static final int DRIVE_MOTOR = 1; // untested
+                public static final int TURN_MOTOR = 2; // untested
+                public static final int TURN_ENCODER = 0; // untested
             }
 
             public static final class FrontRight {
-                public static final int DRIVE_MOTOR = 3;
-                public static final int TURN_MOTOR = 4;
-                public static final int TURN_ENCODER = 1;
+                public static final int DRIVE_MOTOR = 3; // untested
+                public static final int TURN_MOTOR = 4; // untested
+                public static final int TURN_ENCODER = 1; // untested
             }
 
             public static final class BackLeft {
-                public static final int DRIVE_MOTOR = 5;
-                public static final int TURN_MOTOR = 6;
-                public static final int TURN_ENCODER = 2;
+                public static final int DRIVE_MOTOR = 5; // untested
+                public static final int TURN_MOTOR = 6; // untested
+                public static final int TURN_ENCODER = 2; // untested
             }
 
             public static final class BackRight {
-                public static final int DRIVE_MOTOR = 7;
-                public static final int TURN_MOTOR = 8;
-                public static final int TURN_ENCODER = 3;
+                public static final int DRIVE_MOTOR = 7; // untested
+                public static final int TURN_MOTOR = 8; // untested
+                public static final int TURN_ENCODER = 3; // untested
             }
         }
 
         public static final class PID {
             public static final class DriveMotors {
-                public static final TunableNumber kP = new TunableNumber("Drivetrain", "driveKP", 3.3657);
-                public static final TunableNumber kI = new TunableNumber("Drivetrain", "driveKI", 0.0);
-                public static final TunableNumber kD = new TunableNumber("Drivetrain", "driveKD", 0.0);
-                public static final double kS = 0.12817;
-                public static final double kV = 2.7653;
-                public static final double kA = 0.29499;
+                public static final TunableNumber kP = new TunableNumber("Drivetrain", "driveKP", 3.3657); // untested
+                public static final TunableNumber kI = new TunableNumber("Drivetrain", "driveKI", 0.0); // untested
+                public static final TunableNumber kD = new TunableNumber("Drivetrain", "driveKD", 0.0); // untested
+                public static final double kS = 0.12817; // untested
+                public static final double kV = 2.7653; // untested
+                public static final double kA = 0.29499; // untested
             }
 
             public static final class TurnMotors {
-                public static final TunableNumber kP = new TunableNumber("Drivetrain", "turnKP", 0.4);
-                public static final TunableNumber kI = new TunableNumber("Drivetrain", "turnKI", 0.0);
-                public static final TunableNumber kD = new TunableNumber("Drivetrain", "turnKD", 0.01);
+                public static final TunableNumber kP = new TunableNumber("Drivetrain", "turnKP", 0.4); // untested
+                public static final TunableNumber kI = new TunableNumber("Drivetrain", "turnKI", 0.0); // untested
+                public static final TunableNumber kD = new TunableNumber("Drivetrain", "turnKD", 0.01); // untested
             }
 
             public static final class Trajectories {
-                public static final double xkP = 1.5;
-                public static final double ykP = 1.5;
-                public static final double thetakP = 1.5;
+                public static final double xkP = 1.5; // untested
+                public static final double ykP = 1.5; // untested
+                public static final double thetakP = 1.5; // untested
             }
 
             public static final class TurnToAngle {
-                public static final double kP = 0.9;
-                public static final double kI = 0;
-                public static final double kD = 0;
+                public static final double kP = 0.9; // untested
+                public static final double kI = 0; // untested
+                public static final double kD = 0; // untested
             }
 
-            public static final class TurnToBall {
-                public static final double kP = 1;
-                public static final double kI = 0;
-                public static final double kD = 0;
-            }
-
-            public static final class TurnToGoal {
-                public static final double kP = 1;
-                public static final double kI = 0;
-                public static final double kD = 0;
-            }
         }
 
         public static final class Offsets {
-            public static final double FRONT_LEFT = -4.654;
-            public static final double FRONT_RIGHT = -3.465;
-            public static final double BACK_LEFT = -4.433;
-            public static final double BACK_RIGHT = -4.580;
+            public static final double FRONT_LEFT = -4.654; // untested
+            public static final double FRONT_RIGHT = -3.465; // untested
+            public static final double BACK_LEFT = -4.433; // untested
+            public static final double BACK_RIGHT = -4.580; // untested
         }
 
         public static final class Geometry {
             /** Distance between centers of right and left wheels on robot. */
-            public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(18.9);
+            public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(18.9); // untested
             /** Distance between front and back wheels on robot. */
-            public static final double WHEEL_BASE_METERS = Units.inchesToMeters(18.9);
-            public static final double GEARING = 1.0 / 6.75;
-            public static final double WHEEL_RADIUS_METERS = 0.048;
+            public static final double WHEEL_BASE_METERS = Units.inchesToMeters(18.9); // untested
+            public static final double GEARING = 1.0 / 6.75; // untested
+            public static final double WHEEL_RADIUS_METERS = 0.048; // untested
             public static final double WHEEL_CIRCUMFERENCE = 2.0 * Math.PI * WHEEL_RADIUS_METERS;
             public static final double ENCODER_DISTANCE_TO_METERS = WHEEL_CIRCUMFERENCE * GEARING;
 
-            public static final double MAX_PHYSICAL_VELOCITY_METERS_PER_SECOND = 4.42;
-            public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = Math.PI / 4;
-            public static final double MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 2 * Math.PI;
+            public static final double MAX_PHYSICAL_VELOCITY_METERS_PER_SECOND = 4.42; // untested
+            public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = Math.PI / 4; // untested
+            public static final double MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 2 * Math.PI; // untested
 
             public static final class Turning {
                 public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = 5 * Math.PI;
@@ -139,8 +128,8 @@ public final class Constants {
     }
 
     public static final class Teleop {
-        public static double PERCENT_LIMIT = 0.60;
-        public static final double SLOW_MODE_PERCENT_LIMIT = 0.20;
+        public static double PERCENT_LIMIT = 0.60; // untested
+        public static final double SLOW_MODE_PERCENT_LIMIT = 0.20; // untested
         public static final boolean IS_JOYSTICK_INPUT_RATE_LIMITED = true;
 
         /**
@@ -150,22 +139,33 @@ public final class Constants {
          * A rate limit of 3, for example, means that 0->1 in 1/3 sec.
          * Larger numbers mean less of a rate limit.
          */
-        public static final double JOYSTICK_INPUT_RATE_LIMIT = 15.0;
+        public static final double JOYSTICK_INPUT_RATE_LIMIT = 15.0; // untested
 
     }
 
     public static final class Vision {
-        public static final String CAMERA_NAME = "OV9281-01";
-        public static final Transform3d ROBOT_TO_CAM = new Transform3d(
-                new Translation3d(0.24, 0.0, 0.0),
-                new Rotation3d(0, 0, 0));
+        public static final String[] CAMERA_NAMES = new String[] { "OV9281-01", "OV9281-02", "OV9281-03", "OV9281-04" };
+        public static final Transform3d[] ROBOT_TO_CAMS = new Transform3d[] {
+                new Transform3d(
+                        new Translation3d(0.24, 0.0, 0.0), // untested
+                        new Rotation3d(0, 0, 0)), // untested
+                new Transform3d(
+                        new Translation3d(0.24, 0.0, 0.0), // untested
+                        new Rotation3d(0, 0, 0)), // untested
+                new Transform3d(
+                        new Translation3d(0.24, 0.0, 0.0), // untested
+                        new Rotation3d(0, 0, 0)), // untested
+                new Transform3d(
+                        new Translation3d(0.24, 0.0, 0.0), // untested
+                        new Rotation3d(0, 0, 0)), // untested
+        };
     }
 
     public static final class Auton {
-        public static final double MAX_VELOCITY = 1;
-        public static final double MAX_ACCELERATION = 1;
-        public static final double MAX_ANGULAR_VELOCITY = 6 * Math.PI;
-        public static final double MAX_ANGULAR_ACCELERATION = 8 * Math.PI;
+        public static final double MAX_VELOCITY = 1; // untested
+        public static final double MAX_ACCELERATION = 1; // untested
+        public static final double MAX_ANGULAR_VELOCITY = 6 * Math.PI; // untested
+        public static final double MAX_ANGULAR_ACCELERATION = 8 * Math.PI; // untested
 
     }
 }
