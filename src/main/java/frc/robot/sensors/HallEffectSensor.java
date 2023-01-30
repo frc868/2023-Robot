9b488881ac11bc375249e161a4aee66a4bc6e434
ZorzalG <nickwillimJT2@gmail.com>
2023-01-30 18:13:41 -0500

    created HallEffectSensor class by copying 2019 code

--- src/main/java/frc/robot/sensors/HallEffectSensor.java
+++ src/main/java/frc/robot/sensors/HallEffectSensor.java
@@ -1,7 +1,16 @@
 package frc.robot.sensors;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
-public class HallEffectSensor {
+public class HallEffectSensor extends SubsystemBase{
     
+    private DigitalInput limitSwitch;
+    
+    public HallEffectSensor(int channel){
+        this.limitSwitch = new DigitalInput(channel);
+    }
+
+    public boolean get() {
+        return this.limitSwitch.get();
+    }
 }
