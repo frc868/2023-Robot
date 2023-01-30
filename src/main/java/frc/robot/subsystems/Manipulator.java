23ecc6e30a2b8f6d783902c570ed35367ad9be1a
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2023-01-30 18:02:35 -0500

    added author comment

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -2,6 +2,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * The manipulator class
+ * 
+ * @author gc
+ */
 public class Manipulator extends SubsystemBase {
     
 }
