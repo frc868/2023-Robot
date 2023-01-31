34b54e0c4065eeb8c51fd4d686c86948dc6a94f4
videogamedunkey <88414608+videogamedunkey@users.noreply.github.com>
2023-01-31 18:23:56 -0500

    Update Elevator.java
    Began creating the necessary motor objects and PID controller.

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.XboxController;
+
 
 /**
  * elevator
@@ -8,6 +14,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * @author hr jt
  */
 
-public class Elevator extends SubsystemBase{
+public class Elevator extends SubsystemBase {
+
+    private Constants constants = new Constants();
+
+    CANSparkMax lSpool = new CANSparkMax(0, MotorType.kBrushless);
+    CANSparkMax rSpool = new CANSparkMax(1, MotorType.kBrushless);
+    PIDController pid = new PIDController(Constants.kP, Constants.kI, Constants.kD);
+
+
+
+    public void actuateElevator() {
+
+        lSpool.set();
+        rSpool.set();
+
+    }
+
+    
     
 }
