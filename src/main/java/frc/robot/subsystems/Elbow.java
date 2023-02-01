db0574ec8877829c5476b95c126bf709b3d6ae2d
GetRolledBozo <114703260+GetRolledBozo@users.noreply.github.com>
2023-01-31 19:25:10 -0500

    PIDSubsystem -> ProfiledPIDSubsystem. Created elbow encoder New getMeasurement method

--- src/main/java/frc/robot/subsystems/Elbow.java
+++ src/main/java/frc/robot/subsystems/Elbow.java
@@ -3,31 +3,36 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import frc.robot.Constants;
 
-public class Elbow extends PIDSubsystem {
-    private CANSparkMax ElbowMotor = new CANSparkMax(Constants.Elbow.CANIDs.ELBOW_MOTOR, MotorType.kBrushless);
-    private ArmFeedforward FeedForward = new ArmFeedforward(Constants.Elbow.FeedForward.kS,
+public class Elbow extends ProfiledPIDSubsystem{
+    private CANSparkMax elbowMotor = new CANSparkMax(Constants.Elbow.CANIDs.ELBOW_MOTOR, MotorType.kBrushless);
+    private ArmFeedforward feedfoward = new ArmFeedforward(Constants.Elbow.FeedForward.kS,
     Constants.Elbow.FeedForward.kG, Constants.Elbow.FeedForward.kV, Constants.Elbow.FeedForward.kA);
-    
-    public Elbow(PIDController controller, double initialPosition) {
-        super(controller, initialPosition);
-        
-    }
+    private AnalogEncoder elbowEncoder = new AnalogEncoder(0); //untested channel
+
+    public Elbow(ProfiledPIDController controller) {
+        super(new ProfiledPIDController(Constants.Elbow.PIDIDs.kP, Constants.Elbow.PIDIDs.kP,
+         Constants.Elbow.PIDIDs.kP, null));
+         getController().setTolerance(1.0);
+        }
 
     @Override
-    protected void useOutput(double output, double setpoint) {
-        // TODO Auto-generated method stub
-        
+    protected void useOutput(double output, State setpoint) {
+        // elbowMotor.setVoltage(output + feedforward.calculate(getMeasurement(), setpoint)); 
     }
+   
+     
 
     @Override
     protected double getMeasurement() {
-        // TODO Auto-generated method stub
-        return 0;
+        return elbowEncoder.getAbsolutePosition();
+        
     }
-
+    
 }
