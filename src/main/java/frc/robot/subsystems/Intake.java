7c11d19239b0ecdf4664849f706a31be64c939ee
TehBamBam <brycerellim@gmail.com>
2023-01-31 21:08:46 -0500

    Better Javadoc comments Switch order of left and right objects

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -18,54 +18,54 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * Intake subsystem, contains two passover motor controllers, passover solenoid,
- * and intake solenoid
+ * and intake solenoid.
  * 
  * @author BAM
  */
 
 public class Intake extends SubsystemBase {
     /**
-     * Creates a new can spark motor controller for the right passover motor
+     * Motors that drive the left passover.
      */
-    private CANSparkMax rightPassoverMotor = new CANSparkMax(Constants.Intake.RIGHT_PASSOVER_MOTOR,
+    private CANSparkMax leftPassoverMotor = new CANSparkMax(Constants.Intake.CANIDs.LEFT_PASSOVER_MOTOR,
             MotorType.kBrushless);
     /**
-     * Creates a new can spark motor controller for the left passover motor
+     * Motors that drive the right passover.
      */
-    private CANSparkMax leftPassoverMotor = new CANSparkMax(Constants.Intake.LEFT_PASSOVER_MOTOR,
+    private CANSparkMax rightPassoverMotor = new CANSparkMax(Constants.Intake.CANIDs.RIGHT_PASSOVER_MOTOR,
             MotorType.kBrushless);
     /**
-     * Creates new Double Solenoid object for the passover wheels
+     * Solenoid that extends and retracts passover wheels.
      */
     private DoubleSolenoid passoverSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,
             6, 7); // untested
     /**
-     * Creates new Double Solenoid object for the intake
+     * Solenoid that extends and retracts the intake system.
      */
     private DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,
             4, 5); // untested
     /**
-     * Creates a motor controller group to allow both passovers to move at once
+     * Motor controller group for the passover motors.
      */
-    MotorControllerGroup passoverMotorController = new MotorControllerGroup(rightPassoverMotor,
-            leftPassoverMotor);
+    MotorControllerGroup passoverMotors = new MotorControllerGroup(leftPassoverMotor,
+            rightPassoverMotor);
     /**
-     * Creates a new Compressor object
+     * Pnuematic Hub will become obsolete after rev updates their vendor rep.
      */
     private PneumaticHub pneumaticHub = new PneumaticHub();
 
     /**
-     * Intake constructor
+     * Constructs the intake system.
      */
     public Intake() {
         rightPassoverMotor.setInverted(true);
         pneumaticHub.enableCompressorDigital();
         LoggingManager.getInstance().addGroup("Intake", new LogGroup(
                 new Logger[] {
-                        new DeviceLogger<CANSparkMax>(rightPassoverMotor, "Right Passover Motor",
-                                LogProfileBuilder.buildCANSparkMaxLogItems(rightPassoverMotor)),
                         new DeviceLogger<CANSparkMax>(leftPassoverMotor, "Left Passover Motor",
                                 LogProfileBuilder.buildCANSparkMaxLogItems(leftPassoverMotor)),
+                        new DeviceLogger<CANSparkMax>(rightPassoverMotor, "Right Passover Motor",
+                                LogProfileBuilder.buildCANSparkMaxLogItems(rightPassoverMotor)),
                         new DeviceLogger<DoubleSolenoid>(intakeSolenoid, "Intake Solenoid",
                                 LogProfileBuilder.buildDoubleSolenoidLogItems(intakeSolenoid)),
                         new DeviceLogger<DoubleSolenoid>(passoverSolenoid, "Passover Solenoid",
@@ -77,44 +77,44 @@ public class Intake extends SubsystemBase {
     }
 
     /**
-     * Sets Passover wheels to the down position
+     * Sets Passover wheels to the down position.
      */
-    public void setPassoverDown() {
+    public void setPassoverExtended() {
         passoverSolenoid.set(Value.kForward); // untested
     }
 
     /**
-     * Sets Passover wheels to the up position
+     * Sets Passover wheels to the up position.
      */
-    public void setPassoverUp() {
+    public void setPassoverRetracted() {
         passoverSolenoid.set(Value.kReverse); // untested
     }
 
     /**
-     * Sets the intake to the down position
+     * Sets the intake to the down position.
      */
     public void setIntakeDown() {
         intakeSolenoid.set(Value.kForward); // untested
     }
 
     /**
-     * Sets the intake to the up position
+     * Sets the intake to the up position.
      */
     public void setIntakeUp() {
         intakeSolenoid.set(Value.kReverse); // untested
     }
 
     /**
-     * Runs the passover motors
+     * Runs the passover motors.
      */
     public void runPassoverMotors() {
-        passoverMotorController.set(.5);
+        passoverMotors.set(.5);
     }
 
     /**
-     * Stops the passover motors
+     * Stops the passover motors.
      */
     public void stopPassoverMotors() {
-        passoverMotorController.set(0);
+        passoverMotors.set(0);
     }
 }
