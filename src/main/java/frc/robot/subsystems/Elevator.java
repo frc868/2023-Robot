1936c002d4325309bcc9fd15b208e5cea00375e8
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-02-04 12:57:08 -0500

    Refactor code
    Update docstrings a bit
    Move the hall effect check to the lowest level it can go
    Change the desired positionc ommand
    Add an enum to describe the levels

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,20 +3,14 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.techhounds.houndutil.houndauto.AutoManager;
+import com.techhounds.houndutil.houndlib.Utils;
 import com.techhounds.houndutil.houndlog.LogGroup;
 import com.techhounds.houndutil.houndlog.LogProfileBuilder;
 import com.techhounds.houndutil.houndlog.LoggingManager;
@@ -27,96 +21,153 @@ import frc.robot.Constants;
 /**
  * The elevator subsystem, with two motors and motion profling.
  * 
- * @author hr jt
+ * @author hr
+ * @author jt
  */
 
-public class Elevator extends ProfiledPIDSubsystem{
-    /** The top motor. */
-    private CANSparkMax primary = new CANSparkMax(Constants.Elevator.CANIDs.PRIMARY_MOTOR, MotorType.kBrushless);
+public class Elevator extends ProfiledPIDSubsystem {
+    public static enum ElevatorPosition {
+        BOTTOM(0),
+        CONE_LOW(0),
+        CONE_MID(0),
+        CONE_HIGH(0),
+        CUBE_LOW(0),
+        CUBE_MID(0),
+        CUBE_HIGH(0),
+        HUMAN_PLAYER(0),
+        TOP(0);
 
-    /** The bottom motor. */
-    private CANSparkMax secondary = new CANSparkMax(Constants.Elevator.CANIDs.SECONDARY_MOTOR, MotorType.kBrushless);
+        public final int value;
 
-    /** Limit switch that represents max extension.  */
-    private DigitalInput topLimit = new DigitalInput(Constants.Elevator.CANIDs.ELEVATOR_TOP);
+        private ElevatorPosition(int value) {
+            this.value = value;
+        }
+    }
 
-    /** Limit switch that represents minimum extension. */
-    private DigitalInput bottomLimit = new DigitalInput(Constants.Elevator.CANIDs.ELEVATOR_BOTTOM);
+    /** The left motor of the elevator. */
+    private CANSparkMax leftMotor = new CANSparkMax(Constants.Elevator.CANIDs.PRIMARY_MOTOR, MotorType.kBrushless);
 
-    /** Feedforward controller. */
-    private SimpleMotorFeedforward elevatorFeedforward = new SimpleMotorFeedforward(Constants.Elevator.PID.kS, Constants.Elevator.PID.kV);
+    /** The right motor of the elevator. */
+    private CANSparkMax rightMotor = new CANSparkMax(Constants.Elevator.CANIDs.SECONDARY_MOTOR, MotorType.kBrushless);
 
-    /** Initialization code. */
-    public Elevator(){
+    /**
+     * The object that controlls both elevator motors.
+     */
+    private MotorControllerGroup motors = new MotorControllerGroup(leftMotor, rightMotor);
+
+    /** Hall effect sensor that represents the minimum extension of the elevator. */
+    private DigitalInput bottomHallEffect = new DigitalInput(Constants.Elevator.CANIDs.ELEVATOR_BOTTOM);
+
+    /** Hall effect sensor that represents the maximum extension of the elevator. */
+    private DigitalInput topHallEffect = new DigitalInput(Constants.Elevator.CANIDs.ELEVATOR_TOP);
+
+    /**
+     * The feedforward controller (calculates voltage based on the setpoint's
+     * velocity and acceleration).
+     */
+    private SimpleMotorFeedforward elevatorFeedforward = new SimpleMotorFeedforward(Constants.Elevator.PID.kS,
+            Constants.Elevator.PID.kV);
+
+    /**
+     * Used to make sure that the PID controller does not enable unless the elevator
+     * has been zeroed off of the hall effects.
+     */
+    private boolean isInitialized = false;
+
+    /** Initializes the elevator. */
+    public Elevator() {
         super(new ProfiledPIDController(Constants.Elevator.PID.kP.get(),
                 Constants.Elevator.PID.kI.get(),
                 Constants.Elevator.PID.kD.get(),
                 new TrapezoidProfile.Constraints(Constants.Elevator.MAX_VELOCITY,
-                    Constants.Elevator.MAX_ACCELERATION)));
-            
-            LoggingManager.getInstance().addGroup("Elevator", new LogGroup(
+                        Constants.Elevator.MAX_ACCELERATION)));
+
+        LoggingManager.getInstance().addGroup("Elevator", new LogGroup(
                 new Logger[] {
-                        new DeviceLogger<CANSparkMax>(primary, "Primary Elevator Motor",
-                            LogProfileBuilder.buildCANSparkMaxLogItems(primary)),
-                        new DeviceLogger<CANSparkMax>(secondary, "Secondary Elevator Motor",
-                                LogProfileBuilder.buildCANSparkMaxLogItems(secondary))
+                        new DeviceLogger<CANSparkMax>(leftMotor, "Primary Elevator Motor",
+                                LogProfileBuilder.buildCANSparkMaxLogItems(leftMotor)),
+                        new DeviceLogger<CANSparkMax>(rightMotor, "Secondary Elevator Motor",
+                                LogProfileBuilder.buildCANSparkMaxLogItems(rightMotor))
                 }));
     }
 
+    /**
+     * Uses the output from the ProfiledPIDController. Adds the output to the result
+     * of the calculation from the feedforward controller.
+     * 
+     * @param output   the output of the ProfiledPIDController
+     * @param setpoint the setpoint state of the ProfiledPIDController, for
+     *                 feedforward
+     */
     @Override
     public void useOutput(double output, TrapezoidProfile.State setpoint) {
         double feedforward = elevatorFeedforward.calculate(setpoint.position, setpoint.velocity);
-        primary.setVoltage(output+feedforward);
-        secondary.setVoltage(output+feedforward);
+        leftMotor.setVoltage(output + feedforward);
+        rightMotor.setVoltage(output + feedforward);
     }
 
+    /**
+     * Returns the measurement of the process variable used by the
+     * ProfiledPIDController.
+     * 
+     * @return the output of the left motor's encoder
+     */
     @Override
-    public double getMeasurement(){
-        return primary.getEncoder().getPosition();
+    public double getMeasurement() {
+        return leftMotor.getEncoder().getPosition();
+    }
+
+    /**
+     * Checks if the elevator is at its goal position.
+     * 
+     * @return true if the elevator is at its goal position
+     */
+    public boolean isAtGoal() {
+        return getController().atGoal();
     }
 
     /** Sets the value of each encoder to the minimum distance. */
-    public void resetEncoders(){
-        primary.getEncoder().setPosition(Constants.Elevator.MIN_LENGTH);
-        secondary.getEncoder().setPosition(Constants.Elevator.MIN_LENGTH);
+    public void resetEncoders() {
+        leftMotor.getEncoder().setPosition(0);
+        rightMotor.getEncoder().setPosition(0);
     }
 
     /** Stops both motors. */
-    public void stop(){
-        primary.set(0);
-        secondary.set(0);
+    public void stop() {
+        leftMotor.stopMotor();
+        rightMotor.stopMotor();
     }
 
-    /** Sets the speed of each motors, while neither limit switch has been activated. */
-    public void setSpeed(double speed){
-        if(topLimit.get() && bottomLimit.get()){
-            primary.set(speed);
-            secondary.set(speed);
-        }
-        else{
-            stop();
-        }
-    }
-
-    /** Moves the elevator to a desired position.
+    /**
+     * Sets the speed of the motors. Will refuse to go past the bottom or top points
+     * of the elevator.
      * 
-     * 
-     * @param position the extension distance in meters.
+     * @param speed the speed, from -1.0 to 1.0
      */
-    public Command setElevatorPositionCommand(double position){
-        return new RunCommand(() -> {
-            if(topLimit.get() && bottomLimit.get()){
-                this.setGoal(position);
-            }
-            else{
-                stop();
-            }
-        });
+    public void setSpeed(double speed) {
+        if (!Utils.limitMechanism(bottomHallEffect.get(), topHallEffect.get(), speed))
+            motors.set(speed);
     }
 
-    
+    /**
+     * Sets the voltage of the motors. Will refuse to go past the bottom or top
+     * points of the elevator.
+     * 
+     * @param speed the speed, from -1.0 to 1.0
+     */
+    public void setVoltage(double voltage) {
+        if (!Utils.limitMechanism(bottomHallEffect.get(), topHallEffect.get(), voltage))
+            motors.setVoltage(voltage);
+    }
 
-    
-
-
+    /**
+     * Returns an InstantCommand that sets the goal position of the elbow, and
+     * enables the controller.
+     * 
+     * @param position an ElevatorPosition to set the elbow to
+     * @return the command
+     */
+    public Command setDesiredPositionCommand(ElevatorPosition position) {
+        return runOnce(() -> setGoal(position.value)).andThen(this::enable);
+    }
 }
