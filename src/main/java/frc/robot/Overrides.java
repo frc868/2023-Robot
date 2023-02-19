95a40c8fc2c150bb0da64cf205b8154fd9a97437
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-02-19 11:27:25 -0500

    Add overrides

--- src/main/java/frc/robot/Overrides.java
+++ src/main/java/frc/robot/Overrides.java
@@ -1,17 +1,51 @@
 package frc.robot;
 
+import java.util.function.BooleanSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-public class Overrides {
-    private static boolean OPERATOR_OVERRIDE = false;
+public enum Overrides {
+    FRONT_LEFT_DRIVE_MOTOR_DISABLE(false),
+    FRONT_LEFT_TURN_MOTOR_DISABLE(false),
+    FRONT_RIGHT_DRIVE_MOTOR_DISABLE(false),
+    FRONT_RIGHT_TURN_MOTOR_DISABLE(false),
+    BACK_LEFT_DRIVE_MOTOR_DISABLE(false),
+    BACK_LEFT_TURN_MOTOR_DISABLE(false),
+    BACK_RIGHT_DRIVE_MOTOR_DISABLE(false),
+    BACK_RIGHT_TURN_MOTOR_DISABLE(false),
 
-    public static CommandBase operatorOverrideCommand() {
-        return Commands.startEnd(() -> OPERATOR_OVERRIDE = true, () -> OPERATOR_OVERRIDE = false)
-                .withName("Operator Override");
+    ELEVATOR_LEFT_MOTOR_DISABLE(false),
+    ELEVATOR_RIGHT_MOTOR_DISABLE(false),
+    ELBOW_MOTOR_DISABLE(false),
+    PASSOVER_LEFT_MOTOR_DISABLE(false),
+    PASSOVER_RIGHT_MOTOR_DISABLE(false),
+
+    MANUAL_MECH_CONTROL_MODE(false),
+    SAFETIES_DISABLE(true),
+    SPEED_LIMITS_DISABLE(true),
+    MECH_LIMITS_DISABLE(true),
+    DRIVER_EMERGENCY_MODE(false);
+
+    private boolean isEnabled;
+
+    private Overrides(boolean isEnabled) {
+        this.isEnabled = isEnabled;
     }
 
-    public static boolean isOperatorOverridden() {
-        return OPERATOR_OVERRIDE;
+    public boolean getStatus() {
+        return this.isEnabled;
+    }
+
+    public BooleanSupplier getStatusSupplier() {
+        return () -> this.isEnabled;
+    }
+
+    public CommandBase enableC() {
+        return Commands.runOnce(() -> this.isEnabled = true);
+    }
+
+    public CommandBase disableC() {
+        return Commands.runOnce(() -> this.isEnabled = true);
     }
 }
