ce6b1ec3c0d82b6cd573bd29608d6120d85be49b
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2023-03-11 08:00:57 -0600

    Reduce move back distance on auto cube score

--- src/main/java/frc/robot/commands/Scoring.java
+++ src/main/java/frc/robot/commands/Scoring.java
@@ -143,10 +143,10 @@ public class Scoring {
                                 GamePiece.CUBE,
                                 drivetrain.moveDeltaPathFollowingCommand(
                                         new Transform2d(
-                                                new Translation2d(0.4, 0),
+                                                new Translation2d(0.25, 0),
                                                 new Rotation2d()),
-                                        new PathConstraints(2,
-                                                1))
+                                        new PathConstraints(4,
+                                                3))
                                         .andThen(Commands.sequence(
                                                 manipulator.setPincersReleasedCommand(gamePieceSupplier)))),
                         gamePieceSupplier::get));
